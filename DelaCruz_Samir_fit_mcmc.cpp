debe682da280989b20cc9b7b54f963a5acf63624
Sas2o <samjo1306@hotmail.com>
2019-04-10 20:23:48 -0500

    nel_______________________________________________

---
+++ DelaCruz_Samir_fit_mcmc.cpp
@@ -0,0 +1,84 @@
+#include <fstream>
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <cmath>
+#include <random>
+
+using namespace std; 
+
+int leerArchivo(string nombreArchivo, double* &z);
+float f(double* c, int gradoPoli, float x_obs);
+float logProbabilidad(double* c, int gradoPoli);
+void MCMC_polinominal(int n_pasos);
+
+double* x = NULL;
+double* y = NULL;
+int n_xy;
+
+int main(){
+  n_xy=leerArchivo("valores_x.txt", x);
+  n_xy=leerArchivo("valores_y.txt", y);
+  srand48(time(0));
+  MCMC_polinominal(1000000);
+  
+  return 0;
+}
+
+float f(double* c, int gradoPoli, float x_obs){
+  float y_ajust=0;
+  for (int n=0; n<gradoPoli+1;n++){
+    y_ajust+=c[n]*pow(x_obs,n);
+  }
+  return y_ajust;
+}
+float logProbabilidad(double* c, int gradoPoli){
+  float logprobabilidad=0;
+  for (int i=0;i<n_xy;i++){
+    logprobabilidad+=-pow(y[i]-f(c, gradoPoli, x[i]), 2);
+  }
+  return logprobabilidad;
+}
+void MCMC_polinominal(int n_pasos){
+  float dp=0.1;
+  float alpha,r;
+  double* antPaso=new double[4];
+  for (int j=0;j<n_pasos;j++){
+    double* sigPaso=new double[4];
+    for (int k=0;k<4;k++){
+      sigPaso[k]=antPaso[k] + 2*(drand48()-0.5)*dp;
+    }
+    alpha=drand48();
+    r=exp(logProbabilidad(sigPaso,3)-logProbabilidad(antPaso,3));
+    if (r>1){
+      r=1;      
+    }
+    if (alpha<r){
+      antPaso=sigPaso;
+    }
+    cout << antPaso[0] << " " << antPaso[1] << " " << antPaso[2] << " " << antPaso[3] << endl;
+  }
+}
+
+int leerArchivo(string nombreArchivo, double* &z){
+  ifstream infile; 
+  string line;
+  int n = 0;
+
+  infile.open(nombreArchivo);
+  while(infile){
+    n+=1;
+    getline(infile, line);
+  }
+  infile.close();
+
+  infile.open(nombreArchivo);
+  z=new double[n];
+  for (int i=0; i<n;i++){
+    getline(infile,line);
+    z[i]=atof(line.c_str());
+  }
+  infile.close();
+
+  return n;
+}
---
+++ fit_mcmc.py
@@ -0,0 +1,59 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+puntos = 30
+print('Empezamos! puntos: {}'.format(puntos))
+filename = "DelaCruz_Samir_fit_mcmc.cpp"
+
+#define los nombres de los archivos
+execname = filename.split(".")[0]+".x"
+dataname = filename.split(".")[0]+".dat"
+plotname = filename.split(".")[0]+".png"
+
+# compila
+a = os.system("g++ {} -o {}".format(filename, execname))
+puntos += 10
+print('compilo! puntos: {}'.format(puntos))
+
+# ejecuta
+a = os.system("./{} > {}".format(execname, dataname))
+puntos += 10
+print('ejecuto! puntos: {}'.format(puntos))
+
+# lee los datos observacionales
+x = np.loadtxt("valores_x.txt")
+y = np.loadtxt("valores_y.txt")
+
+# lee los datos del montecarlo
+mcmc_data = np.loadtxt(dataname)
+puntos += 10
+print('leyo los resultados! puntos: {}'.format(puntos))
+
+#calcula la media de cada columna
+params = np.mean(mcmc_data, axis=0)
+puntos += 10
+print('calculo la media de cada columna! puntos: {}'.format(puntos))
+
+# a partir de esos resultados calcula el fit
+def model(x, params):
+    y = np.zeros(len(x))
+    for i in range(len(params)):
+        y = y + (params[i] * x**i)
+    return y
+x_model = np.linspace(x.min(), x.max(), 100)
+y_model = model(x_model, params)
+puntos += 10
+print('calculo el fit! puntos: {}'.format(puntos))
+
+# prepara la grafica
+plt.figure(figsize=(5,5))
+plt.errorbar(x, y, yerr=0.1, fmt='o', label='data')
+plt.plot(x_model, y_model, label='mcmc fit')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.legend()
+plt.savefig(plotname, bbox_inches='tight')
+puntos += 10
+print('hizo una grafica! puntos: {}'.format(puntos))
+print('Si la grafica es algo razonable habra 10 puntos mas')
